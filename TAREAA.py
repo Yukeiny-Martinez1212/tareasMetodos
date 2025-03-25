b9aa6638123a1a6891635bd6ee94777487ab7322
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2025-03-25 00:14:52 -0500

    Add files via upload

---
+++ TAREAA.py
@@ -0,0 +1,27 @@
+import math
+N1 = 1
+N2 = 4
+print (N1+N2)
+print (N1-N2)
+print (N1*N2)
+print (N1/N2)
+
+#RAIZ 
+M = math.sqrt(4)
+print (M)
+#SENO
+M1 = math.sin(math.pi/2)
+print (M1)
+#LOG 
+M2 = math.exp(math.log(10))
+print (M2)
+M3 = math.exp (3/4)
+print (M3)
+M4 = math.factorial
+print (M4)
+M5 = 1/math.inf 
+print (M5)
+M6 = math.inf*2 
+print (M6)
+m7 = math.inf/math.inf 
+print(m7)
