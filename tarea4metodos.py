600c24f0e4f36448b456faf5e9c74d22e9069223
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2025-03-25 00:15:58 -0500

    Add files via upload

---
+++ tarea4metodos.py
@@ -0,0 +1,61 @@
+ 
+y = 15
+print (y)
+
+texto = "hola mundo"
+repeticiones = texto.count("o")
+print(repeticiones)
+
+x = 20
+print (x)
+
+x = x+4
+print(x)
+
+x = 2
+y = x+2
+print (x)
+print (y)
+
+s="S"
+print(s)
+
+w = "6"
+len (w)
+
+w = "hola mundo"
+print (w)
+
+texto = "hola mundo"
+print (texto[6]) 
+
+texto = "hola mundo"
+print (texto[6], texto[9]) 
+
+texto = "hola mundo"
+print(texto[5:])
+
+texto = "hola mundo"
+print(texto[:4])
+
+texto = "hola mundo"
+print(texto[6-2:6])
+
+texto = "hola mundo"
+print(texto[::2])
+
+print("hola " + "mundo")
+
+texto = "hola mundo"
+nuevo_texto = texto.replace("hola mundo", "adios mundo")
+print(nuevo_texto) 
+
+nombre = "Juan"  
+texto = "hola mundo"
+nuevo_texto = texto.replace("mundo", nombre) 
+print(nuevo_texto)
+
+
+
+
+
