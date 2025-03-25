de94d402c996b91a64a7779ee4bb2b1717d07580
Yukeiny-Martinez1212 <101664228+Yukeiny-Martinez1212@users.noreply.github.com>
2025-03-25 00:15:13 -0500

    Add files via upload

---
+++ TAREA3.py
@@ -0,0 +1,22 @@
+print("I love Python!")
+s = "S"
+w = "hello world"
+type (s)
+print (w [6])
+print (w [6:11])
+print (w [6:])
+print (w [6:])
+print (w [6:])
+print (w [:5])
+print (w [6:-2])
+print (w [::2])
+P1 = "PALABRA1"
+P2 = "PALABRA2"
+print (P1+P2)
+X=1 
+print (" X = " + str(X) )
+
+
+
+
+
